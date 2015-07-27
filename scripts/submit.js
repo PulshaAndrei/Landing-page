b56162e8a838c0e16442fc8cfc679be9300f4b90
Pulsha Andrei <ap.ttnby@gmail.com>
2015-07-27 21:33:25 +0300

    5

--- scripts/submit.js
+++ scripts/submit.js
@@ -45,6 +45,24 @@ function send(form){
 		verify = false;
 	}
 	if (verify){
+		var xmlhttp = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+		xmlhttp.open('POST', 'https://mandrillapp.com/api/1.0/messages/send.json');
+		xmlhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+		xmlhttp.onreadystatechange = function() {
+		    if (xmlhttp.readyState == 4) {
+		        if(xmlhttp.status == 200) alert('Mail sended!')
+		        else if(xmlhttp.status == 500) alert('Check apikey')
+		        else alert('Request error');
+		    }
+		}
+		xmlhttp.send(JSON.stringify({'key': 'jJvyiaMWRKRJeUOoMc_aDA',
+		   'message': {
+		       'from_email': 'mail_from@write.here',
+		       'to': [{'email': 'mail_to@write.her', 'type': 'to'}],
+		       'autotext': 'true',
+		       'subject': 'Yeah!',
+		       'html': '<h1>Its work!</h1>'
+		    }}));
 		$('.close').click();
 	}
 }
