dc74c67c18a7608c0d5ae105d30cf37fc9f80739
Pulsha Andrei <ap.ttnby@gmail.com>
2015-07-27 21:25:25 +0300

    3

--- scripts/submit.js
+++ scripts/submit.js
@@ -8,10 +8,12 @@ $(function(){
 
 
 function send(form){
+	var verify=true;
 	var reg = /.+@.+\..+/i;
 	if (!reg.test(form.email.value)) {
 		$('.email-div input').addClass('error_input');
 		$('.email-div p').addClass('error_p');
+		verify = false;
 	}
 	else{
 		$('.email-div input').removeClass('error_input');
@@ -20,6 +22,7 @@ function send(form){
 	if (form.name.value == ""){
 		$('.name-div input').addClass('error_input');
 		$('.name-div p').addClass('error_p');
+		verify = false;
 	}
 	else{
 		$('.name-div input').removeClass('error_input');
@@ -28,12 +31,22 @@ function send(form){
 	if (form.message.value == ""){
 		$('.message-div textarea').addClass('error_input');
 		$('.message-div p').addClass('error_p');
+		verify = false;
 	}
 	else{
 		$('.message-div textarea').removeClass('error_input');
 		$('.message-div p').removeClass('error_p');
 	};
-	if (grecaptcha.getResponse()=="") $('.popup').addClass('disable_popup');
+	if (grecaptcha.getResponse()=="") {
+		$('.captcha-div p').removeClass('error_p');
+	};
+	else{
+		$('.captcha-div p').addClass('error_p');
+		verify = false;
+	}
+	if verify{
+		$('.close').click();
+	}
 }
 
 
